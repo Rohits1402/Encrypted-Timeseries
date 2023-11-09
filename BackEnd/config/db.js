566de07f83289df36e3ec5b69e757350c3904989
Rohit <rschauhan1402@gmail.com>
2023-11-09 21:51:58 +0530

    mongo

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const configureDB = async () => {
   try {
     const db = await mongoose.connect(
-      "mongodb://127.0.0.1:27017/encrypted-timeseries"
+      "mongodb+srv://rschauhan1402:rohit2001@cluster0.e8cccei.mongodb.net/"
     );
     console.log("connecting to db");
   } catch (e) {
